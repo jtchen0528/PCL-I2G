8de7023b953c1c31ce13fca8ac83e20bafefa693
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-10 13:59:03 +0800

    udpate

--- scripts/ff_i2G.sh
+++ scripts/ff_i2G.sh
@@ -1,19 +1,20 @@
 ### Deepfakes processing ###
 
-# train 
-python -m data.processing.faceforensics_process_frames_I2G \
-	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
-	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
-	--output_dir ~/Documents/AIIU/Datasets/faceforensics/original_aligned
+# # train 
+# python -m data.processing.faceforensics_process_frames_I2G \
+# 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+# 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
+# 	--output_dir ~/Documents/AIIU/Datasets/faceforensics/original_aligned
 	
 # # val
-# python -m data.processing.faceforensics_process_frames_I2G \
-# 	--source_dir_original /scratch3/users/ycliu/dataset/faceforensics/video/raw/real \
-# 	--outsize 256 --split /scratch2/users/jtchen0528/FF++/splits/val.json \
-# 	--output_dir /scratch2/users/jtchen0528/FF++/real
+python -m data.processing.faceforensics_process_frames_I2G \
+	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/val.json \
+	--output_dir ~/Documents/AIIU/Datasets/faceforensics/original_aligned
 
-# # test
+# # # test
 # python -m data.processing.faceforensics_process_frames_I2G \
-# 	--source_dir_original /scratch3/users/ycliu/dataset/faceforensics/video/raw/real \
-# 	--outsize 256 --split /scratch2/users/jtchen0528/FF++/splits/test.json \
-# 	--output_dir /scratch2/users/jtchen0528/FF++/real
+# 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+# 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/test.json \
+# 	--output_dir ~/Documents/AIIU/Datasets/faceforensics/original_aligned
+	
