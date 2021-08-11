72249ce75924ffcea6d101a5bbe4275e66bdea72
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 09:25:01 +0800

    update

--- scripts/ff_i2G.sh
+++ scripts/ff_i2G.sh
@@ -18,9 +18,23 @@
 # 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/test.json \
 # 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/original
 
-# # train
+# # # train
+# python -m data.processing.faceforensics_process_frames_I2G_manipulated \
+# 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+# 	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
+# 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
+# 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF
+
+# # val
 python -m data.processing.faceforensics_process_frames_I2G_manipulated \
 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
-	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
+	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/val.json \
 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF
+
+# # # test
+# python -m data.processing.faceforensics_process_frames_I2G_manipulated \
+# 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+# 	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
+# 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/test.json \
+# 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF
