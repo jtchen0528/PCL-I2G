c9653a26cb1d338ffbf9af77c9d181dcf40bd003
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 09:19:38 +0800

    update

--- scripts/ff_i2G.sh
+++ scripts/ff_i2G.sh
@@ -1,10 +1,10 @@
 ### Deepfakes processing ###
 
-# # train 
-python -m data.processing.faceforensics_process_frames_I2G \
-	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
-	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
-	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/original
+# # # train 
+# python -m data.processing.faceforensics_process_frames_I2G \
+# 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+# 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
+# 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/original
 	
 # # val
 # python -m data.processing.faceforensics_process_frames_I2G \
@@ -17,3 +17,10 @@ python -m data.processing.faceforensics_process_frames_I2G \
 # 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 # 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/test.json \
 # 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/original
+
+# # train
+python -m data.processing.faceforensics_process_frames_I2G \
+	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
+	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
+	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF
