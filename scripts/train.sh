98e4b94f57c45a9422756ddf82714b1282573755
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-12 01:41:29 +0800

    iupdate

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,11 +5,11 @@ dset=/scratch3/users/clairelai/faceforensics_aligned
 # dset=datasets-sample/celebahq
 # dset=~/Documents/AIIU/Datasets/CNNDetection
 # dset=~/Documents/AIIU/Datasets/FaceForensics++
-gpu=8
+gpu=7
 
 python train_I2G.py \
 	--gpu_ids $gpu --seed 0 --loadSize 128 --fineSize 128 \
-	--name PCL-I2G-FF128-32frames-5e-5 --save_epoch_freq 10 \
+	--name PCL-I2G-FF128-32frames-Modified-5e-5 --save_epoch_freq 10 \
  	--real_im_path $dset/Deepfakes/original \
  	--fake_im_path $dset/Deepfakes/manipulated \
 	--which_model_netD resnet34_layer4_extra3 --model patch_inconsistency_discriminator --lbda 10 \
