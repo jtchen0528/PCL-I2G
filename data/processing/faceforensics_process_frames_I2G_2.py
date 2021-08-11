f24cab7705d6d90ca92f5553d5c7ed59a83e089b
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 15:11:11 +0800

    udpate

--- data/processing/faceforensics_process_frames_I2G_2.py
+++ data/processing/faceforensics_process_frames_I2G_2.py
@@ -1,4 +1,5 @@
 import argparse
+from genericpath import isfile
 import os
 import numpy as np
 from tqdm import tqdm
@@ -47,42 +48,44 @@ for i, s in enumerate(tqdm(split)):
 
     counter = 0
     for j, (frame, orig) in enumerate(zip(video_frames, original_video_frames)):
-        frame_path_DF = os.path.join(args.source_dir_manipulated, 'DF', vidname, frame)
-        frame_path_F2F = os.path.join(args.source_dir_manipulated, 'F2F', vidname, frame)
-        frame_path_FS = os.path.join(args.source_dir_manipulated, 'FS', vidname, frame)
-        frame_path_NT = os.path.join(args.source_dir_manipulated, 'NT', vidname, frame)
-        orig_frame_path = os.path.join(args.source_dir_original, vidname_orig, orig)
-        frame_DF = io.imread(frame_path_DF)
-        print('DF success %s' % frame_path_DF)
-        frame_F2F = io.imread(frame_path_F2F)
-        print('F2F success %s' % frame_path_F2F)
-        frame_FS = io.imread(frame_path_FS)
-        print('FS success %s' % frame_path_FS)
-        frame_NT = io.imread(frame_path_NT)
-        print('NT success %s' % frame_path_NT)
-        orig = io.imread(orig_frame_path)
         try:
             # might return none or out of bounds error
                 # use original landmarks
+
+            frame_path_DF = os.path.join(args.source_dir_manipulated, 'DF', vidname, frame)
+            frame_path_F2F = os.path.join(args.source_dir_manipulated, 'F2F', vidname, frame)
+            frame_path_FS = os.path.join(args.source_dir_manipulated, 'FS', vidname, frame)
+            frame_path_NT = os.path.join(args.source_dir_manipulated, 'NT', vidname, frame)
+            orig_frame_path = os.path.join(args.source_dir_original, vidname_orig, orig)
+            frame_DF = io.imread(frame_path_DF)
+            orig = io.imread(orig_frame_path)
             cropped_orig, landmarks = celebahq_crop(orig)
             cropped_orig = cropped_orig.resize((args.outsize, args.outsize), Image.LANCZOS)
+            if isfile(frame_path_F2F):
+                frame_F2F = io.imread(frame_path_F2F)
+                cropped_F2F = (celebahq_crop(frame_F2F, landmarks)[0]
+                            .resize((args.outsize, args.outsize), Image.LANCZOS))
+                cropped_F2F.save(os.path.join(outdir, 'F2F', split_name,
+                                        '%s_%03d.png' % (vidname, j)))
+            if isfile(frame_path_FS):
+                frame_FS = io.imread(frame_path_FS)
+                cropped_FS = (celebahq_crop(frame_FS, landmarks)[0]
+                            .resize((args.outsize, args.outsize), Image.LANCZOS))
+                cropped_FS.save(os.path.join(outdir, 'FS', split_name,
+                                        '%s_%03d.png' % (vidname, j)))
+            if isfile(frame_path_NT):
+                frame_NT = io.imread(frame_path_NT)
+                cropped_NT = (celebahq_crop(frame_NT, landmarks)[0]
+                            .resize((args.outsize, args.outsize), Image.LANCZOS))
+                cropped_NT.save(os.path.join(outdir, 'NT', split_name,
+                                        '%s_%03d.png' % (vidname, j)))
             cropped_DF = (celebahq_crop(frame_DF, landmarks)[0]
                         .resize((args.outsize, args.outsize), Image.LANCZOS))
-            cropped_F2F = (celebahq_crop(frame_F2F, landmarks)[0]
-                        .resize((args.outsize, args.outsize), Image.LANCZOS))
-            cropped_FS = (celebahq_crop(frame_FS, landmarks)[0]
-                        .resize((args.outsize, args.outsize), Image.LANCZOS))
-            cropped_NT = (celebahq_crop(frame_NT, landmarks)[0]
-                        .resize((args.outsize, args.outsize), Image.LANCZOS))
+
             # save the results
             cropped_DF.save(os.path.join(outdir, 'DF', split_name,
                                       '%s_%03d.png' % (vidname, j)))
-            cropped_F2F.save(os.path.join(outdir, 'F2F', split_name,
-                                      '%s_%03d.png' % (vidname, j)))
-            cropped_FS.save(os.path.join(outdir, 'FS', split_name,
-                                      '%s_%03d.png' % (vidname, j)))
-            cropped_NT.save(os.path.join(outdir, 'NT', split_name,
-                                      '%s_%03d.png' % (vidname, j)))
+
             cropped_orig.save(os.path.join(outdir, 'original', split_name,
                                            '%s_%03d.png' % (vidname, j)))
             # np.savez(os.path.join(outdir, 'detections', split_name,
