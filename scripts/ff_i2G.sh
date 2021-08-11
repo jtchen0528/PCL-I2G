4ad64d29ac19779f2918cc96936c9ccd9adaa12d
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 14:45:54 +0800

    udpate

--- scripts/ff_i2G.sh
+++ scripts/ff_i2G.sh
@@ -2,22 +2,22 @@
 
 # # # train 
 python -m data.processing.faceforensics_process_frames_I2G_2 \
-	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/F2F \
+	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/FS \
 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 	--outsize 256 \
-	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/F2F \
+	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/FS \
 	--split resources/splits/train.json
 
 python -m data.processing.faceforensics_process_frames_I2G_2 \
-	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/F2F \
+	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/FS \
 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 	--outsize 256 \
-	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/F2F \
+	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/FS \
 	--split resources/splits/val.json
 
 python -m data.processing.faceforensics_process_frames_I2G_2 \
-	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/F2F \
+	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/FS \
 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 	--outsize 256 \
-	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/F2F \
+	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/FS \
 	--split resources/splits/test.json
