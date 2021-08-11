0a813c19fd8da813fe3c7c74ee4611f4c6095b0b
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 15:54:56 +0800

    udpate

--- scripts/train.sh
+++ scripts/train.sh
@@ -9,9 +9,9 @@ gpu=8
 
 python train_I2G.py \
 	--gpu_ids $gpu --seed 0 --loadSize 128 --fineSize 128 \
-	--name TEST-PCL-I2G-FF128-5e-5 --save_epoch_freq 1 \
+	--name PCL-I2G-FF128-32frames-5e-5 --save_epoch_freq 10 \
  	--real_im_path $dset/Deepfakes/original \
  	--fake_im_path $dset/Deepfakes/manipulated \
 	--which_model_netD resnet34_layer4_extra3 --model patch_inconsistency_discriminator --lbda 10 \
-	--patience 5 --lr_policy constant --max_epochs 50 --batch_size 512 --lr 5e-5 \
+	--patience 5 --lr_policy constant --max_epochs 1000 --batch_size 512 --lr 5e-5 \
 	--overwrite_config
