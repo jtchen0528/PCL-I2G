53ebf4ba08d6b29b9b466b00810a1820319e3ccd
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 09:20:45 +0800

    update

--- scripts/ff_i2G.sh
+++ scripts/ff_i2G.sh
@@ -19,7 +19,7 @@
 # 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/original
 
 # # train
-python -m data.processing.faceforensics_process_frames_I2G \
+python -m data.processing.faceforensics_process_frames_I2G_manipulated \
 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
 	--outsize 256 --split ~/Documents/AIIU/ForgeryTasks/Pair-Wise-Self-Consistency-Learning-Inconsistency-Image-Generator/resources/splits/train.json \
