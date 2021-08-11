fbdb1139767c1c7b1c37b9497a625caca6cbfaf3
Jhao Ting Chen <jtchen0528@gmail.com>
2021-08-11 14:42:24 +0800

    udpate

--- scripts/ff_i2G.sh
+++ scripts/ff_i2G.sh
@@ -1,9 +1,23 @@
 ### Deepfakes processing ###
 
 # # # train 
+# python -m data.processing.faceforensics_process_frames_I2G_2 \
+# 	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
+# 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+# 	--outsize 256 \
+# 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF \
+# 	--split resources/splits/train.json
+
 python -m data.processing.faceforensics_process_frames_I2G_2 \
 	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
 	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
 	--outsize 256 \
 	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF \
-	--split resources/splits/train.json
+	--split resources/splits/val.json
+
+python -m data.processing.faceforensics_process_frames_I2G_2 \
+	--source_dir_manipulated ~/Documents/AIIU/Datasets/faceforensics/DF \
+	--source_dir_original ~/Documents/AIIU/Datasets/faceforensics/original \
+	--outsize 256 \
+	--output_dir ~/Documents/AIIU/Datasets/faceforensics_aligned/DF \
+	--split resources/splits/test.json
